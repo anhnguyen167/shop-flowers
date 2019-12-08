f8c0a760108f871cc2ec6faa44ed404e386b4452
Admin <Admin@192.168.1.180>
2019-12-08 23:28:55 +0700

    cuongvn fix commit conflict -_-

--- src/java/DAO/BasketDAO.java
+++ src/java/DAO/BasketDAO.java
@@ -1,220 +1,3 @@
-<<<<<<< HEAD
-package DAO;
-
-import BEAN.Basket;
-import BEAN.BasketDetail;
-import BEAN.User;
-import DB.connectDB;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class BasketDAO extends DAOConnector {
-
-    private Connection con;
-    private BasketDetailDAO basketDetailDAO;
-
-    public BasketDAO() {
-        getInstance();
-        con = connectDB.connectDB();
-        basketDetailDAO = new BasketDetailDAO();
-    }
-
-    // Thêm mới 1 giỏ hàng
-    public boolean insertBasket(Basket basket) {
-        String sql = "INSERT INTO `baskets`(`user_id`, `booking_date`, `total`, `address`, `state`) VALUES (?,?,?,?,?)";
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            String date = basket.getBooking_date().toString();
-            java.util.Date utilDate = sdf.parse(date);
-            java.sql.Date sqlDate = new Date(utilDate.getTime());
-            PreparedStatement ps = con.prepareStatement(sql);
-            System.out.println(sqlDate);
-            ps.setInt(1, basket.getUser().getId());
-            ps.setDate(2, sqlDate);
-            ps.setInt(3, basket.getTotal());
-            ps.setString(4, basket.getAddress());
-            ps.setInt(5, basket.getState());
-            ps.executeUpdate();
-            return true;
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (ParseException ex) {
-            Logger.getLogger(BasketDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return false;
-    }
-    public int getLastId(){
-        int last_id = 0;
-        String sql = "SELECT MAX(id) FROM `baskets`";
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                last_id = rs.getInt(1);
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(BasketDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return last_id;
-    }
-    public Basket getLastBasket(int last_id) {
-        Basket basket = null;
-        String sql = "SELECT * FROM `baskets` WHERE `id`=?";
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setInt(1, last_id);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                int id = rs.getInt(1);
-                int user_id = rs.getInt(2);
-                Date booking_date = rs.getDate(3);
-                int total = rs.getInt(4);
-                String address = rs.getString(5);
-                int state = rs.getInt(6);
-                UserDAO userDAO = new UserDAO();
-                User user = userDAO.getUserById(user_id);
-                BasketDetailDAO basketDetailDAO = new BasketDetailDAO();
-                ArrayList<BasketDetail> listBasketDetails = basketDetailDAO.getBasketDetailByBasketId(id);
-                basket = new Basket(id, user, booking_date, listBasketDetails, total,state, address);
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(BasketDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return basket;
-    }
-
-    // Thêm mới 1 giỏ hàng
-
-    public boolean createBasket(Basket basket) {
-        String sql = "INSERT INTO `baskets`(`user_id`, `booking_date`, `total`) VALUES (?, ?, 0)";
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            String date = "1000-10-10";
-            java.util.Date utilDate = sdf.parse(date);
-            java.sql.Date sqlDate = new Date(utilDate.getTime());
-            PreparedStatement ps = con.prepareStatement(sql);
-            System.out.println(sqlDate);
-            ps.setInt(1, basket.getUser().getId());
-            ps.setDate(2, sqlDate);
-            ps.executeUpdate();
-            return true;
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (ParseException ex) {
-            Logger.getLogger(BasketDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return false;
-    }
-
-    // Cập nhật giỏ hàng
-    public boolean updateBasket(Basket basket) {
-        String sql = "UPDATE `baskets` SET `booking_date`=?,`total`=? WHERE `id`=?";
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setDate(1, (Date) basket.getBooking_date());
-            ps.setInt(2, basket.getTotal());
-            ps.setInt(3, basket.getId());
-            ps.executeUpdate(sql);
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-	// Khi bắt đầu mỗi lần đăng nhập mới cần kiểm tra xem giỏ hàng của khách hàng này đã có chưa
-    // Nếu có rồi thì booking_date của giỏ hàng sẽ ở dạng 1000-10-10 và trả về giỏ hàng này
-    // Nếu chưa có thì tạo một giỏ hàng mới
-    public Basket getBasketByUserId(User user) {
-        Basket basket = null;
-        String sql = "SELECT * FROM `baskets` WHERE `user_id`=? AND `booking_date`='1000-10-10'";
-        PreparedStatement ps;
-        try {
-            ps = con.prepareStatement(sql);
-            ps.setInt(1, user.getId());
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                int id = rs.getInt(1);
-
-                ArrayList<BasketDetail> listBasketDetail = basketDetailDAO.getBasketDetailByBasketId(id);
-                //basket = new Basket(id, user, rs.getDate(3), listBasketDetail, rs.getInt(4));
-            }
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        return basket;
-    }
-
-    public static ArrayList<Basket> getAllBasket() {
-        Connection con = connectDB.connectDB();
-        ArrayList<Basket> listBasket = new ArrayList<Basket>();
-        String sql = "SELECT * FROM `baskets` WHERE state=1";
-        PreparedStatement ps;
-        try {
-            ps = con.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                int id = rs.getInt(1);
-                int user_id = rs.getInt(2);
-                Date booking_date = rs.getDate(3);
-                int total = rs.getInt(4);
-                String address = rs.getString(5);
-                int state = rs.getInt(6);
-                UserDAO userDAO = new UserDAO();
-                User user = userDAO.getUserById(user_id);
-                BasketDetailDAO basketDetailDAO = new BasketDetailDAO();
-                ArrayList<BasketDetail> listBasketDetails = basketDetailDAO.getBasketDetailByBasketId(id);
-                Basket basket = new Basket(id, user, booking_date, listBasketDetails, total,state, address);
-                listBasket.add(basket);
-            }
-
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        return listBasket;
-
-    }
-    public boolean updateState(Basket basket) {
-        String sql = "UPDATE `baskets` SET state=? WHERE id=?";
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setInt(1,0);
-            ps.setInt(2, basket.getId());
-            
-            int s = ps.executeUpdate();
-            System.out.println("anh xau xi");
-            System.out.println(s);
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public static void main(String[] args) {
-        
-//         BasketDAO dao = new BasketDAO();
-//         Basket basket =dao.getLastBasket(1);
-//         if(dao.updateState(basket)==true){
-//             System.out.println("OK");
-//         }
-    }
-}
-=======
 package DAO;
 
 import BEAN.Basket;
@@ -242,6 +25,23 @@ public class BasketDAO extends connectDB {
         basketDetailDAO = new BasketDetailDAO();
     }
 
+    public boolean updateState(Basket basket) {
+        String sql = "UPDATE `baskets` SET state=? WHERE id=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, 0);
+            ps.setInt(2, basket.getId());
+
+            int s = ps.executeUpdate();
+            System.out.println("anh xau xi");
+            System.out.println(s);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     // Thêm mới 1 giỏ hàng
     public boolean insertBasket(Basket basket) {
         String sql = "INSERT INTO `baskets`(`user_id`, `booking_date`, `total`, `address`, `state`) VALUES (?,?,?,?,?)";
@@ -267,7 +67,8 @@ public class BasketDAO extends connectDB {
         }
         return false;
     }
-    public int getLastId(){
+
+    public int getLastId() {
         int last_id = 0;
         String sql = "SELECT MAX(id) FROM `baskets`";
         try {
@@ -281,6 +82,7 @@ public class BasketDAO extends connectDB {
         }
         return last_id;
     }
+
     public Basket getLastBasket(int last_id) {
         Basket basket = null;
         String sql = "SELECT * FROM `baskets` WHERE `id`=?";
@@ -299,7 +101,7 @@ public class BasketDAO extends connectDB {
                 User user = userDAO.getUserById(user_id);
                 BasketDetailDAO basketDetailDAO = new BasketDetailDAO();
                 ArrayList<BasketDetail> listBasketDetails = basketDetailDAO.getBasketDetailByBasketId(id);
-                basket = new Basket(id, user, booking_date, listBasketDetails, total,state, address);
+                basket = new Basket(id, user, booking_date, listBasketDetails, total, state, address);
             }
         } catch (SQLException ex) {
             Logger.getLogger(BasketDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -308,7 +110,6 @@ public class BasketDAO extends connectDB {
     }
 
     // Thêm mới 1 giỏ hàng
-
     public boolean createBasket(Basket basket) {
         String sql = "INSERT INTO `baskets`(`user_id`, `booking_date`, `total`) VALUES (?, ?, 0)";
         try {
@@ -347,7 +148,7 @@ public class BasketDAO extends connectDB {
         return false;
     }
 
-	// Khi bắt đầu mỗi lần đăng nhập mới cần kiểm tra xem giỏ hàng của khách hàng này đã có chưa
+    // Khi bắt đầu mỗi lần đăng nhập mới cần kiểm tra xem giỏ hàng của khách hàng này đã có chưa
     // Nếu có rồi thì booking_date của giỏ hàng sẽ ở dạng 1000-10-10 và trả về giỏ hàng này
     // Nếu chưa có thì tạo một giỏ hàng mới
     public ArrayList<Basket> getBasketByUserId(User user) {
@@ -392,7 +193,7 @@ public class BasketDAO extends connectDB {
                 User user = userDAO.getUserById(user_id);
                 BasketDetailDAO basketDetailDAO = new BasketDetailDAO();
                 ArrayList<BasketDetail> listBasketDetails = basketDetailDAO.getBasketDetailByBasketId(id);
-                Basket basket = new Basket(user, booking_date, listBasketDetails, total,state, address);
+                Basket basket = new Basket(user, booking_date, listBasketDetails, total, state, address);
                 listBasket.add(basket);
             }
 
@@ -414,4 +215,3 @@ public class BasketDAO extends connectDB {
         dao.createBasket(basket);
     }
 }
->>>>>>> 5595d6256d49f7aff825c3454c8969eb2420784c
--- src/java/DAO/BasketDetailDAO.java
+++ src/java/DAO/BasketDetailDAO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package DAO;
 
 import BEAN.Basket;
@@ -99,112 +98,113 @@ public class BasketDetailDAO extends DAOConnector{
 		}
 		return listBasketDetails;
 	}
+
         public static void main(String[] args) {
             BasketDetailDAO dao = new BasketDetailDAO();
             
     }
 }
-=======
-package DAO;
-
-import BEAN.Basket;
-import BEAN.BasketDetail;
-import BEAN.Product;
-import DB.connectDB;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-
-
-public class BasketDetailDAO extends connectDB{
-	private Connection con;
-	private ProductDAO productDAO;
-	public BasketDetailDAO(){
-		con = connectDB.connectDB();
-		productDAO = new ProductDAO();
-	}
-	
-	
-	// Thêm một sản phẩm vào trong giỏ hàng
-	public boolean insertProductIntoBasket(BasketDetail basketDetail, Basket basket) {
-		String sql = "INSERT INTO `basket_details`(`product_id`, `quantity`, `basket_id`) VALUES (?,?,?)";
-		try {
-			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setInt(1, basketDetail.getProduct().getId());
-			ps.setInt(2, basketDetail.getQuantity());
-			ps.setInt(3, basket.getId());
-			ps.executeUpdate();
-			return true;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-	
-	
-	// Cập nhật số lượng của sản phẩm
-	public boolean updateBasketDetail(BasketDetail basketDetail) {
-		String sql = "UPDATE `basket_details` SET `quantity`=? WHERE `id`=?";
-		try {
-			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setInt(1, basketDetail.getQuantity());
-			ps.setInt(2, basketDetail.getId());
-			ps.executeUpdate(sql);
-			return true;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-	
-	// Xóa sản phẩm khỏi giỏ hàng
-	public boolean deleteBasketDetail(BasketDetail basketDetail) {
-		String sql = "DELETE FROM `basket_details` WHERE `id`=?";
-		try {
-			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setInt(1, basketDetail.getId());
-			ps.executeUpdate(sql);
-			return true;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-	
-	
-	// Lấy các sản phẩm thuộc cùng 1 giỏ hàng
-	public ArrayList<BasketDetail> getBasketDetailByBasketId(int basketId){
-		ArrayList<BasketDetail> listBasketDetails = new ArrayList<BasketDetail>();
-		String sql = "SELECT * FROM `basket_details` WHERE `basket_id`=?";
-		try {
-			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setInt(1, basketId);
-			ResultSet rs = ps.executeQuery();
-			while(rs.next()) {
-				int id = rs.getInt(1);
-				int product_id = rs.getInt(2);
-				int quantity = rs.getInt(3);
-				Product product = productDAO.getProductById(product_id);
-				BasketDetail basketDetail = new BasketDetail(id, product, quantity);
-				listBasketDetails.add(basketDetail);
-			}
-			
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return listBasketDetails;
-	}
-        public static void main(String[] args) {
-            BasketDetailDAO dao = new BasketDetailDAO();
-            System.out.println(dao.getBasketDetailByBasketId(2));
-    }
-}
->>>>>>> 5595d6256d49f7aff825c3454c8969eb2420784c
+//=======
+//package DAO;
+//
+//import BEAN.Basket;
+//import BEAN.BasketDetail;
+//import BEAN.Product;
+//import DB.connectDB;
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.util.ArrayList;
+//
+//
+//
+//public class BasketDetailDAO extends connectDB{
+//	private Connection con;
+//	private ProductDAO productDAO;
+//	public BasketDetailDAO(){
+//		con = connectDB.connectDB();
+//		productDAO = new ProductDAO();
+//	}
+//	
+//	
+//	// Thêm một sản phẩm vào trong giỏ hàng
+//	public boolean insertProductIntoBasket(BasketDetail basketDetail, Basket basket) {
+//		String sql = "INSERT INTO `basket_details`(`product_id`, `quantity`, `basket_id`) VALUES (?,?,?)";
+//		try {
+//			PreparedStatement ps = con.prepareStatement(sql);
+//			ps.setInt(1, basketDetail.getProduct().getId());
+//			ps.setInt(2, basketDetail.getQuantity());
+//			ps.setInt(3, basket.getId());
+//			ps.executeUpdate();
+//			return true;
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return false;
+//	}
+//	
+//	
+//	// Cập nhật số lượng của sản phẩm
+//	public boolean updateBasketDetail(BasketDetail basketDetail) {
+//		String sql = "UPDATE `basket_details` SET `quantity`=? WHERE `id`=?";
+//		try {
+//			PreparedStatement ps = con.prepareStatement(sql);
+//			ps.setInt(1, basketDetail.getQuantity());
+//			ps.setInt(2, basketDetail.getId());
+//			ps.executeUpdate(sql);
+//			return true;
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return false;
+//	}
+//	
+//	// Xóa sản phẩm khỏi giỏ hàng
+//	public boolean deleteBasketDetail(BasketDetail basketDetail) {
+//		String sql = "DELETE FROM `basket_details` WHERE `id`=?";
+//		try {
+//			PreparedStatement ps = con.prepareStatement(sql);
+//			ps.setInt(1, basketDetail.getId());
+//			ps.executeUpdate(sql);
+//			return true;
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return false;
+//	}
+//	
+//	
+//	// Lấy các sản phẩm thuộc cùng 1 giỏ hàng
+//	public ArrayList<BasketDetail> getBasketDetailByBasketId(int basketId){
+//		ArrayList<BasketDetail> listBasketDetails = new ArrayList<BasketDetail>();
+//		String sql = "SELECT * FROM `basket_details` WHERE `basket_id`=?";
+//		try {
+//			PreparedStatement ps = con.prepareStatement(sql);
+//			ps.setInt(1, basketId);
+//			ResultSet rs = ps.executeQuery();
+//			while(rs.next()) {
+//				int id = rs.getInt(1);
+//				int product_id = rs.getInt(2);
+//				int quantity = rs.getInt(3);
+//				Product product = productDAO.getProductById(product_id);
+//				BasketDetail basketDetail = new BasketDetail(id, product, quantity);
+//				listBasketDetails.add(basketDetail);
+//			}
+//			
+//			
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return listBasketDetails;
+//	}
+//        public static void main(String[] args) {
+//            BasketDetailDAO dao = new BasketDetailDAO();
+//            System.out.println(dao.getBasketDetailByBasketId(2));
+//    }
+//}
+//>>>>>>> 5595d6256d49f7aff825c3454c8969eb2420784c
